ce75ed11a99d011a8fa365d55e45f86f2a8f25b2
rms <rooqmeansquare@yandex.ru>
2017-05-10 11:54:59 +0500

    create new csv every hour

--- ftp/get_modbus_data.py
+++ ftp/get_modbus_data.py
@@ -83,104 +83,32 @@ def save_modbus():
 				
 				else:
 					
-					try:																		
-						date = datetime.datetime.now()	
-									
-						for idx, i in enumerate(all_data):							
-							data_list.append(Point(date, idx, i))	
+					try:
+					
+						timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:00")
 							
+						outfilename = '/home/roman/data/{}.csv'.format(timestr)
+								
+						f = open(outfilename, 'a')
+						f.write(str(datetime.datetime.now()))
+						f.write(str(';'))		
+						for i in all_data:
+							f.write(str(i))				 
+							f.write(str(';'))							
+						f.write(str('\n'))		
+						f.close()	
+											
 					except:				
-						write_log('Unable append namedtuple to list (modbus)\n')				
-				
-				
-					if counter >= buffer_size:						
-					
-						#start_time = time.time()
-					
-						counter = 0				
-						threads = [] 
-						fork = True
-						
-						try: 
-							pid = os.fork()
-						
-						except:
-							write_log( "Ошибка создания дочернего процесса" )
-							fork = False
-							
-						else:
-							if pid == 0 : # дочерний процесс
-								copyto_db(data_list)
-								
-								sys.exit( 0 )
-								
-							if pid > 0 :  # родительский процесс
-								#os.wait()							
-								signal.signal(signal.SIGCHLD,signal.SIG_IGN) # игнорируем зомби 
-								
-						
-						#Реинициализация списка
-						data_list = None
-						data_list = []
-					
-						#end_time = time.time()					
-						#print (end_time - start_time)	
-					
+						write_log('Unable save data to file (modbus)\n')				
+	
 					else:											
 						GPIO.output(LED, True)	
 						time.sleep(0.08)					
 						GPIO.output(LED, False)
-											
-					counter += 1		
-		
+					
 					reboot_except_counter = 0
 				
 
-			
-				
-def copyto_db(data):
-
-		labels = ['datetime', 'num_reg', 'data']
-		
-		engine = None
-		
-		try:
-			engine = create_engine('postgresql://roman:1234@localhost:5432/client')					
-			
-			df = pd.DataFrame.from_records(data, columns=labels)
-			df.to_sql('iface_data', engine, if_exists='append', index=False)
-		
-		except:						
-			write_log('Unable save to db \n')	
-			
-			#engine.close()
-			
-			sys.exit( 0 )
-		
-		#else:				
-			#df = pd.read_csv('/home/roman/data/new_data.csv', header=None)
-			#df = pd.read_csv('/home/roman/data/data.csv', sep=';', names = ["datetime", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])						   
-			#df = pd.melt(df.reset_index(), id_vars=['datetime'], var_name=['num_reg'], value_name='data')			
-			#df = df[df.num_reg != 'index'] #убираем строки с появившимся index
-			
-			#conn = psycopg2.connect("dbname='client' user='roman' host='localhost' password='1234'")
-			#cursor = conn.cursor()
-			
-			#with open('/home/roman/data/new_data.csv') as f:
-				#cursor.copy_expert("COPY iface_data(data, datetime, user_login_id, num_reg) FROM STDIN WITH CSV DELIMITER ';'", f)
-			
-			#conn.commit()
-			#cursor.close()			
-		
-		#finally:			
-			# conn.close()				
-			
-		# for i in data:
-			# print(i.datetime, i.num_reg, i.value)
-		
-		
-		
-		
 				
 def template_modbus(tty, addr, baudrate, bytesize, stopbits, timeout, num_reg, qty, functioncode):
 
